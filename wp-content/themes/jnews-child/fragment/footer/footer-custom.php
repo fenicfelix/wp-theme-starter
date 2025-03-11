fe87b2e01ca0c37bf8da08656b082bcccc63ef22
Felix Ogucha <fenicfelix@gmail.com>
2025-03-11 06:17:30 +0000

    Updates footer

--- wp-content/themes/jnews-child/fragment/footer/footer-custom.php
+++ wp-content/themes/jnews-child/fragment/footer/footer-custom.php
@@ -1,54 +1,25 @@
-        <div class="footer-holder" id="footer" data-id="footer">
-            <?php
-            $footer_style = get_theme_mod('jnews_footer_style', '1');
-            if ($footer_style === 'custom') {
-                get_template_part('fragment/footer/footer-custom');
-            } else {
-                get_template_part('fragment/footer/footer-' . $footer_style);
-            }
-            ?>
+<div class="jeg_footer jeg_footer_custom">
+    <div class="jeg_container">
+        <div class="jeg_content">
+            <div class="jeg_vc_content">
+                <?php
+                $footer_builder = JNews\Footer\FooterBuilder::getInstance();
+                $footer_builder->set_on_footer();
+                $footer_builder->render_footer();
+                $footer_builder->not_on_footer();
+                ?>
+            </div>
         </div>
-
-        <div class="jscroll-to-top">
-            <a href="#back-to-top" class="jscroll-to-top_link"><i class="fa fa-angle-up"></i></a>
-        </div>
-        </div>
-
+    </div>
+    <div class="jnews_mobile_sticky_ads">
         <?php
-
-        if (apply_filters('jnews_can_render_account_popup', false)) {
-            get_template_part('fragment/account/account-popup');
+        if (is_front_page()) {
+            echo do_shortcode('[dynamic_ads ad_position="homepage_sticky_footer_ad"]');
+        } else if (is_single()) {
+            echo do_shortcode('[dynamic_ads ad_position="default_post_sticky_footer_ad"]');
+        } else {
+            echo do_shortcode('[dynamic_ads ad_position="default_page_sticky_footer_ad"]');
         }
-
-        wp_footer();
         ?>
-        <div id='zone_1623503952'></div>
-        <script>
-            (function(w, d, o, g, r, a, m) {
-                var cid = 'zone_1623503952';
-                w[r] = w[r] || function() {
-                    (w[r + 'l'] = w[r + 'l'] || []).push(arguments)
-                };
-
-                function e(b, w, r) {
-                    if ((w[r + 'h'] = b.pop()) && !w.ABN) {
-                        var a = d.createElement(o),
-                            p = d.getElementsByTagName(o)[0];
-                        a.async = 1;
-                        a.src = 'https://cdn.' + w[r + 'h'] + '/libs/e.js';
-                        a.onerror = function() {
-                            e(g, w, r)
-                        };
-                        p.parentNode.insertBefore(a, p)
-                    }
-                }
-                e(g, w, r);
-                w[r](cid, {
-                    id: 1623503952,
-                    domain: w[r + 'h']
-                });
-            })(window, document, 'script', ['ftd.agency'], 'ABNS');
-        </script>
-        </body>
-
-        </html>
+    </div>
+</div><!-- /.footer -->
